cff135d347cce58c6b3983abe78a9d86aa06e737
Dushan Saputhanthri <dushan.s@eyepax.com>
2022-05-20 17:59:05 +0530

    Initial Commit

---
+++ ReadiCloudStorage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReadiCloudStorage
+//
+//  Created by Dushan Saputhanthri on 5/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
